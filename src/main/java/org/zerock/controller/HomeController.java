ee48ba411bd9fcaa9e44f50b1f6c4b62da559b65
5CLASS-184 <5CLASS-184@HB-jongro30>
2018-05-14 16:38:13 +0900

    h2

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	//jinju zzang~~~~
-    //hello = hi
+    //hello = hi = h2
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
